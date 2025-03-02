37be84f67569210901a07535047c029a2ec42c1c
Den-the-baller <Ballerstein@arizona.edu>
2025-03-01 21:09:57 -0700

    updated logic to create 16*16 squares vs just 16. also added row vs column class distinction

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
-let i = 0;
+let row = 0;
+let column = 0;
+
 const mainDiv = document.querySelector("div")
-while (i < 16){
-
+while (column < 16 ){    
     let addDiv = document.createElement("div")
-    addDiv.className = "square"
-    mainDiv.appendChild(addDiv)
-    console.log("Div added")
-    i++
-
+        addDiv.className = "square column"
+        mainDiv.appendChild(addDiv)
+        console.log("Column added")
+        column++
+        
+    while (row < 16){
+        let addDiv = document.createElement("div")
+        addDiv.className = "square row"
+        mainDiv.appendChild(addDiv)
+        console.log("Row added")
+        row++
+    }
+    row = 0;
 }
